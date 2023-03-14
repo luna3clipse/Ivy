18de02269be39e3cf154694fdeeeb2bf9e95ebe0
visionaryww <110308641+visionaryww@users.noreply.github.com>
2023-03-14 13:26:59 -0600

    use apiv2 for check_ranked

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,10 @@
 prefix = "."
-token = "."
-bot_id = "."
-osukey = "."
+token = ""
+bot_id = ""
 debug = "1"
+osukey = {
+    'client_id': 99999, #replace
+    'client_secret': 'key', #replace
+    'grant_type': 'client_credentials',
+    'scope': 'public'
+}
