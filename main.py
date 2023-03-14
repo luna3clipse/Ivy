889ea19e5e249294e3c52457a2cba8f1d00bc6df
luna <aatteure@gmail.com>
2023-03-14 03:52:05 +0000

    Create repeat user check using .txt

--- main.py
+++ main.py
@@ -2,40 +2,70 @@ import discord
 import os
 import requests
 
-from mysql.connector import MySQLConnection, Error
-from python_mysql_dbconfig import read_db_config
-
 from discord.ext import commands
 from utils import config, errorHandler, mysqlConfig
+from utils.intHandler import statusValues
 
 bot = commands.Bot(command_prefix=config.prefix, intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
+    os.system('CLS')
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    print(f'Bot is running on {bot.user}')  
-    await bot.load_extension("errorHandler")   
-    await bot.change_presence(status=discord.Status.online, 
+    await bot.load_extension("utils.errorHandler")   
+    await bot.change_presence(status=discord.Status.idle, 
                               activity=discord.Activity(type=discord.ActivityType.watching, 
-                              name="something"))
+                              name="something"))  
+    if bot.user.bot != True:
+        print(f'You are not running on a bot account. ({bot.user})')
+
+    elif config.debug == '1':                           
+        print(f'Bot is running on {bot.user} ({bot.user.id} | {bot.user.bot} | {bot.user.created_at})')
+        print(f'This useless machine is sponsored by Insurance Gang ✉')
+        print(f'I am in love with thick thighed femboys')
+
+    else:
+        print(f'This useless machine is sponsored by Insurance Gang ✉')
+        print(f'I am in love with thick thighed femboys')
 
 @bot.command(aliases=['check', 'checkrank'])
 async def check_ranked(ctx, username):
     req = requests.get(f"https://osu.ppy.sh/api/get_beatmaps?u={username}&since=1999-01-01&k={config.osukey}")
     data = req.json()
 
-    global message
-    message = await ctx.send("*Checking your maps...*")
+    with open('./misc/users.txt') as f:
+        if f'{username}' in f.read():
+            print(f"[{username}] Repeat user found")
+            await ctx.send("You have already verified!")
+        else:
+            global message
+            message = await ctx.send("*Checking your maps...*")
       
     while True:
         for i in data:
             if i['approved'] != '1':
-                continue
+                if config.debug == '1':
+                    bid = i['beatmapset_id']
+                    app = i['approved']
+                    status = statusValues[app]
+                    print(f"{username} | {bid} | {app} ({status})")
+                    continue
+
+                else:
+                    continue
+
             elif i['approved'] == '1':
                 await message.edit(content=f"Ranked map found! You have received your role, **{username}**.")
-                print(f"[{username}] Ranked map found - giving role")
+                print(f"{username} | Ranked map found - giving role")
+                with open("./misc/users.txt", "a") as f:
+                    f.write(f"\n{username}")
                 break
+
+            else:
+                await message.edit(content=f"You do not have any ranked maps, {username}.")
+
         break
+        
 
 
 bot.run(config.token)
